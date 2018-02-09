5fb01266cff969d67c766d56684e7abd2af9b980
richardcsj <richard.chun91@gmail.com>
2018-02-09 11:27:23 +0000

    fixing package , server for deployment

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3100
+//Install express server
+const express = require('express');
+const app = express();
 
-express()
- 	.use(express.static(path.join(__dirname, 'dist')))
-	.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	  next();
-	})
-  .get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-})
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
