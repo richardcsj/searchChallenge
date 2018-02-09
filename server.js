2200f9c67809f86af793690a0f7de749ad9d0929
richardcsj <richard.chun91@gmail.com>
2018-02-09 09:41:59 +0000

    add server.js and Procfile to deploy to heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 3100
+
+express()
+  .use(express.static(path.join(__dirname, 'dist')))
+ app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
