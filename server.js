b1d0822febe68ee1a05fd2ba8cff45950a1e89fa
richardcsj <richard.chun91@gmail.com>
2018-02-09 09:58:41 +0000

    Added Cors in server.js

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const path = require('path')
 const PORT = process.env.PORT || 3100
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
+ 	.use(express.static(path.join(__dirname, 'dist')))
+	.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	  next();
+	})
   .get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
