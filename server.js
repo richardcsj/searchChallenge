06de2b01a275b84c42e6d24f794a989cdba83227
richardcsj <richard.chun91@gmail.com>
2018-02-09 09:54:39 +0000

    fixed another typo in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3100
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
- app.get('*', function (req, res) {
+  .get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
